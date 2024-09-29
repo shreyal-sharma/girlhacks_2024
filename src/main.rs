9384ae889eca6116e1344cb573b34deb1c4a4378
mc2279github <mc2279@njit.edu>
2024-09-28 20:05:29 -0400

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use http_body_util::Full;
+use hyper::service::service_fn;
+use hyper_util::rt::tokio::{TokioExecutor, TokioIo};
+use hyper_util::server::conn::auto::Builder;
+use rustls_pemfile::{certs, private_key};
+use rustls_pki_types::CertificateDer;
+use std::env;
+use std::fs;
+
+use hyper::body::{Bytes, Incoming};
+use std::io::{self, BufReader};
+use std::sync::Arc;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpListener;
+use tokio_rustls::rustls::ServerConfig;
+use tokio_rustls::TlsAcceptor;
+
+use http::{Method, Request, Response, StatusCode};
+
+async fn service(req: Request<Incoming>) -> io::Result<Response<Full<Bytes>>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "") => Ok(Response::builder()
+            .status(200)
+            .body(Full::from(tokio::fs::read("").await?))
+            .unwrap()),
+        (&Method::POST, "") => Ok(Response::builder().status(200).body(()).unwrap()),
+        _ => Ok(Response::builder().status(404).body(()).unwrap()),
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let addr = args.get(1).unwrap();
+    let key = private_key(&mut BufReader::new(fs::File::open(&args[2])?)).unwrap();
+    let cert: io::Result<Vec<CertificateDer>> =
+        certs(&mut BufReader::new(fs::File::open(&args[3]).unwrap())).collect();
+    let mut config = ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(cert?, key.unwrap())
+        .unwrap();
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
+
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+    let listner = TcpListener::bind(addr).await.unwrap();
+    let service = service_fn(service);
+    loop {
+        let (stream, addr) = listner.accept().await?;
+        let acceptor = acceptor.clone();
+        let fut = async move {
+            let stream = acceptor.accept(stream).await.unwrap();
+
+            let x = Builder::new(TokioExecutor::new())
+                .serve_connection(TokioIo::new(stream), service)
+                .await;
+            Ok(()) as io::Result<()>
+        };
+        tokio::spawn(async move {
+            match fut.await {
+                Ok(o) => {}
+                Err(e) => {}
+            }
+        });
+    }
+}
